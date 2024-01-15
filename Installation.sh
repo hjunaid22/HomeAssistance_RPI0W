46f76298ea7d9342cddc25a33464c786c353ada3
hjunaid22 <h.junaid@hotmail.com>
2024-01-15 09:18:55 -0500

    update

--- Installation.sh
+++ Installation.sh
@@ -6,15 +6,6 @@ echo "alias='ls -al'" >> /etc/profile
 ######## installing Packages ###########
 sudo apt-get install dnsutils #nslookup and dig
 sudo apt-get install apache2
-######
-touch /home/h.junaid/.credentials
-echo "username=download" >> /home/h.junaid/.credentials
-chmod 600
-sudo mkdir /backup
-sudo echo "//192.168.1.5/Open_Share /backup cifs credentials=/home/h.junaid/.credentials,rw,uid=1001,gid=1001" >> /etc/fstab
-sudo groupadd admin
-sudo usermod -aG admin
-########### Docker Installation #######
 apt-get update
 apt-get install ca-certificates curl gnupg -y
 apt-get install vim -y
