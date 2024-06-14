37840d782d96119fc5ef47f41bd464d3c47f9e3c
Junaid Hassan <57299484+hjunaid22@users.noreply.github.com>
2024-06-14 13:10:02 -0400

    Update Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -4,9 +4,9 @@ read "hostname"
 
 echo "alias='ls -al'" >> /etc/profile
 ######## installing Packages ###########
-sudo apt-get install dnsutils #nslookup and dig
-sudo apt-get install apache2
-apt-get update
+sudo apt-get install dnsutils -y #nslookup and dig
+sudo apt-get install apache2 -y
+apt-get update -y
 apt-get install ca-certificates curl gnupg -y
 apt-get install vim -y
 ####### Installing Docker ############
@@ -17,7 +17,7 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-apt-get upgrade
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+apt-get upgrade -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 docker ps
